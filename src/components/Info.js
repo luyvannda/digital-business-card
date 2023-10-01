16de51962205a73dff00e19e04753f9f3610ef04
luyvannda <philips.joe329@gmail.com>
2023-10-01 16:07:16 +0700

    style the info component according to the style guide

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,6 +4,9 @@ export default function Info() {
   return (
     <div className="info">
       <img className="profile-photo" src={photo} alt="a smiling web developer"></img>
+      <h3>Luy Vannda</h3>
+      <p className="job-title">Frontend Developer</p>
+      <p><a href="https://github.com/luyvannda" rel="noreferrer" target="_blank">github.com/luyvannda</a></p>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import "@fontsource/inter";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
