b75be21b2d035026f38899a88387e160fbeed28e
luyvannda <philips.joe329@gmail.com>
2023-10-02 16:11:41 +0700

    add font awesome to the project and style email and social icon

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,4 +1,9 @@
 import photo from "./images/profile-photo.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+
 
 export default function Info() {
   return (
@@ -7,6 +12,13 @@ export default function Info() {
       <h3>Luy Vannda</h3>
       <p className="job-title">Frontend Developer</p>
       <p><a href="https://github.com/luyvannda" rel="noreferrer" target="_blank">github.com/luyvannda</a></p>
+      <div className="contact">
+        <button className="email-btn"><a href="mailto:luyvannda@gmail.com" rel="noreferrer" target="_blank">
+          <FontAwesomeIcon icon={faEnvelope} className="email-icon" />Email</a></button>
+        <button className="linkedin-btn"><a href="https://www.linkedin.com/in/luyvannda/" rel="noreferrer" target="_blank">
+          <FontAwesomeIcon icon={faLinkedin} className="linkedin-icon" />Linkedin
+        </a></button>
+      </div>
     </div>
   )
 }
