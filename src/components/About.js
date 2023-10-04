eb77b32790efc37a0f9d5fdb08882ef024233a0f
luyvannda <philips.joe329@gmail.com>
2023-10-04 10:42:50 +0700

    complete styling About and Interests component

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,9 @@
+
+
+
 export default function About() {
   return (
-    <div className="about">
+    <div className="about-container">
       <h3>About</h3>
       <p>I am a frontend developer with a particular interest in challenging designs. I try to keep up with accessibility standards and best practices, and am always looking for new things to learn.</p>
     </div>
